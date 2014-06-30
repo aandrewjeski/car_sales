8c14d29d0894e858201422f3a1554b815c35493c
Adam Andrewjeski <aandrewjeski@gmail.com>
2014-06-30 10:26:20 -0400

    added model(name) to cars

---
+++ db/migrate/20140630142234_add_name_to_cars.rb
@@ -0,0 +1,5 @@
+class AddNameToCars < ActiveRecord::Migration
+  def change
+    add_column :cars, :name, :string, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140630135607) do
+ActiveRecord::Schema.define(version: 20140630142234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140630135607) do
     t.string  "color",        null: false
     t.integer "year",         null: false
     t.integer "mileage",      null: false
+    t.string  "name",         null: false
   end
 
   create_table "manufacturers", force: true do |t|
